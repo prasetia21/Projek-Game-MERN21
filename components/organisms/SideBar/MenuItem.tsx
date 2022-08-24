e62407e504215073b3594a0a339698b00ec30b9f
prasetia21 <75926777+prasetia21@users.noreply.github.com>
2022-08-24 13:13:43 +0700

    Refactoring Member
    blm selesai semua, progress masih di member overview, transaction

---
+++ components/organisms/SideBar/MenuItem.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import cx from "classnames";
+
+interface MenuItemProps {
+	title: string;
+	icon:
+		| "ic-menu-overview"
+		| "ic-menu-transaction"
+		| "ic-menu-card"
+		| "ic-menu-rewards"
+		| "ic-menu-message"
+		| "ic-menu-setting"
+		| "ic-menu-logout";
+	active?: boolean;
+	href:string;
+}
+export default function MenuItem(props: Partial<MenuItemProps>) {
+	const { title, icon, active, href } = props;
+	const classItem = cx({
+		item: true,
+		"mb-30": true,
+		active: active,
+	});
+	return (
+		<div className={classItem}>
+			<div className="me-3">
+				<Image
+					src={`/icon/${icon}.svg`}
+					width={25}
+					height={25}
+					alt="icon menu dashboard"
+				/>
+			</div>
+
+			<p className="item-title m-0">
+				<Link href={href}>
+					<a className="text-lg text-decoration-none">{title}</a>
+				</Link>
+			</p>
+		</div>
+	);
+}
