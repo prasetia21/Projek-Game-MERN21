d555e00b6fab021e03916e31ff37ebc01335d5a1
prasetia21 <75926777+prasetia21@users.noreply.github.com>
2022-08-31 20:07:58 +0700

    install axios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,45 @@
-import '../styles/utilities.css'
-import '../styles/homepage.css'
-import '../styles/detail.css'
-import '../styles/checkout.css'
-import '../styles/complete-checkout.css'
-import '../styles/sign-in.css'
-import '../styles/sign-up.css'
-import '../styles/sign-up-photo.css'
-import '../styles/sign-up-photo-success.css'
-import '../styles/sign-up-success.css'
-import '../styles/404-not-found.css'
-import '../styles/overview.css'
-import '../styles/sidebar.css'
-import '../styles/transactions.css'
-import '../styles/transactions-detail.css'
-import '../styles/edit-profile.css'
-import '../styles/navbar-log-in.css'
-import '../styles/bootstrap-5.0.2-dist/css/bootstrap.min.css'
-import {useEffect} from 'react'
+import { Html, Main, NextScript } from "next/document";
+import "../styles/utilities.css";
+import "../styles/homepage.css";
+import "../styles/detail.css";
+import "../styles/checkout.css";
+import "../styles/complete-checkout.css";
+import "../styles/sign-in.css";
+import "../styles/sign-up.css";
+import "../styles/sign-up-photo.css";
+import "../styles/sign-up-photo-success.css";
+import "../styles/sign-up-success.css";
+import "../styles/404-not-found.css";
+import "../styles/overview.css";
+import "../styles/sidebar.css";
+import "../styles/transactions.css";
+import "../styles/transactions-detail.css";
+import "../styles/edit-profile.css";
+import "../styles/navbar-log-in.css";
+import "../styles/bootstrap-5.0.2-dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-  require("../styles/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js");
-}, []);
+    require("../styles/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js");
+  }, []);
 
   return (
-  <>
-    <Head>
-      {/*Google Fonts*/}
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
+    <>
+      <Head>
+        {/*Google Fonts*/}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet" />
+          rel="stylesheet"
+        />
+      </Head>
 
-    </Head>
-    
-    <Component {...pageProps} />
-  </>
-  )
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,30 @@
-import type { NextPage } from 'next'
-import {useEffect} from 'react'
-import Navbar from '../components/organisms/Navbar'
-import MainBanner from '../components/organisms/MainBanner'
-import TransactionStep from '../components/organisms/TransactionStep'
-import FeatureGame from '../components/organisms/FeatureGame'
-import Reached from '../components/organisms/Reached'
-import Story from '../components/organisms/Story'
-import Footer from '../components/organisms/Footer'
-import AOS from 'aos'
-import 'aos/dist/aos.css';
+import type { NextPage } from "next";
+import { useEffect } from "react";
+import Navbar from "../components/organisms/Navbar";
+import MainBanner from "../components/organisms/MainBanner";
+import TransactionStep from "../components/organisms/TransactionStep";
+import FeatureGame from "../components/organisms/FeatureGame";
+import Reached from "../components/organisms/Reached";
+import Story from "../components/organisms/Story";
+import Footer from "../components/organisms/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home: NextPage = () => {
   useEffect(() => {
-     AOS.init();
-   }, [])
+    AOS.init();
+  }, []);
   return (
     <>
-    
-    <Navbar />
-    <MainBanner />
-    <TransactionStep />
-    <FeatureGame />
-    <Reached />
-    <Story />
-    <Footer />
-    
+      <Navbar />
+      <MainBanner />
+      <TransactionStep />
+      <FeatureGame />
+      <Reached />
+      <Story />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
